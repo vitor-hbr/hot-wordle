64830537dad3722da09ca16d745d0f79779d0e13
Vitor Hugo Brasiliense da Rosa <vitor.hbr@outlook.com>
2023-07-21 13:56:23 -0300

    Update GridLetter with state prop

--- svelte-hot-wordle/src/components/GridLetter.svelte
+++ svelte-hot-wordle/src/components/GridLetter.svelte
@@ -3,14 +3,17 @@
 
 	/** @type {string} */
 	export let letter;
-	/** @type {boolean} */
-	export let isActive = false;
+	/** @type {'pending' | 'selected' | 'correct' | 'otherPosition'} */
+	export let state = 'pending';
 </script>
 
 <div
 	class={clsx([
 		'flex aspect-square items-center justify-center rounded border-4 text-4xl font-bold text-white',
-		isActive && 'border-b-8'
+		state === 'pending' && 'border-none bg-gray-500',
+		state === 'selected' && 'border-b-8',
+		state === 'otherPosition' && 'bg-yellow-500',
+		state === 'correct' && 'bg-green-500'
 	])}
 >
 	{letter}
