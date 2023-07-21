26ae4d67eb66f03453af9c824217929dbf007ede
Vitor Hugo Brasiliense da Rosa <vitor.hbr@outlook.com>
2023-07-21 16:55:11 -0300

    feat: added word requests

--- svelte-hot-wordle/src/api/word.service.js
+++ svelte-hot-wordle/src/api/word.service.js
@@ -1,14 +1,43 @@
+const apiBaseUrl = 'https://....';
+
 /**
- * @typedef {Object} AttemptedWordDTO
- * @property {string} letter
- * @property {'pending' | 'selected' | 'correct' | 'otherPosition'} state
+ * @param {number} numberOfLetters - The number of letters for the word.
+ * * @returns {Promise<string>} - Returns the encrypted word.
  */
+export async function getWord(numberOfLetters) {
+	//const res = (await fetch(`${apiBaseUrl}/getWord`)).json()
+
+	return 'teste';
+}
 
 /**
  * @param {string} encryptedWord - The fixed hidden word.
  * @param {string} attempt - The attempt made by the player.
- * * @returns {Promise<boolean>} - True if the player has guessed the hidden word, otherwise false.
+ * * @returns {Promise<AttemptedWord>} - True if the player has guessed the hidden word, otherwise false.
  */
 export async function validateAttempt(encryptedWord, attempt) {
-	return true;
+	//const res = (await fetch(`${apiBaseUrl}/validateAttempt`)).json()
+
+	return [
+		{
+			letter: 'T',
+			state: 'correct'
+		},
+		{
+			letter: 'E',
+			state: 'correct'
+		},
+		{
+			letter: 'S',
+			state: 'correct'
+		},
+		{
+			letter: 'T',
+			state: 'correct'
+		},
+		{
+			letter: 'E',
+			state: 'correct'
+		}
+	];
 }
