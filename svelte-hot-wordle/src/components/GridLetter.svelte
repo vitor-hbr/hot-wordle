b0598a3d5dc001d44e644dc4e54f1e25a360772e
Vitor Hugo Brasiliense da Rosa <vitor.hbr@outlook.com>
2023-07-21 12:22:26 -0300

    feat: added GridLetter WordGrid

---
+++ svelte-hot-wordle/src/components/GridLetter.svelte
@@ -0,0 +1,17 @@
+<script>
+	import clsx from 'clsx';
+
+	/** @type {string} */
+	export let letter;
+	/** @type {boolean} */
+	export let isActive = false;
+</script>
+
+<div
+	class={clsx([
+		'flex aspect-square items-center justify-center rounded border-4 text-4xl font-bold text-white',
+		isActive && 'border-b-8'
+	])}
+>
+	{letter}
+</div>
---
+++ svelte-hot-wordle/src/components/WordGrid.svelte
@@ -0,0 +1,32 @@
+<script>
+	import GridLetter from '../components/GridLetter.svelte';
+
+	export let wordLength = 5;
+	export let letterMatrix = [
+		['A', 'B', 'C', 'D', 'E'],
+		['F', 'G', 'H', 'I', 'J'],
+		['K', 'L', 'M', 'N', 'O'],
+		['P', 'Q', 'R', 'S', 'T'],
+		['U', 'V', 'W', 'X', 'Y'],
+		['Z', 'Z', 'Z', 'Z', 'Z']
+	];
+
+	/** @type {HTMLElement} */
+	let rootElement;
+
+	$: rootElement && rootElement.style.setProperty('--word-length', String(wordLength));
+</script>
+
+<div bind:this={rootElement} class={`grid-cols-letters grid grid-flow-row gap-1`}>
+	{#each letterMatrix as row}
+		{#each row as letter}
+			<GridLetter {letter} />
+		{/each}
+	{/each}
+</div>
+
+<style lang="postcss">
+	.grid-cols-letters {
+		grid-template-columns: repeat(var(--word-length), minmax(0, 1fr));
+	}
+</style>
