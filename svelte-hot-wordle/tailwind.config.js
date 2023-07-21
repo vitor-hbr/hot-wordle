7e8fa31ac1d65be89968ccd1996b0d5efad43c02
Vitor Hugo Brasiliense da Rosa <vitor.hbr@outlook.com>
2023-07-21 10:53:30 -0300

    build: added tailwind to svelte app

---
+++ svelte-hot-wordle/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'outsmart-purple': '#333399'
+			}
+		}
+	},
+	plugins: []
+};
